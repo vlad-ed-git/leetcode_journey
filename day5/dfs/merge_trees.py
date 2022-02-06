13290b2b0f1355e13c267d63def94ebf751c22a0
Vladimir Peter <vlad.edna@gmail.com>
2022-02-06 19:34:46 +0800

    day 5 - added 1n

---
+++ day5/dfs/merge_trees.py
@@ -0,0 +1,34 @@
+"""
+You are given two binary trees root1 and root2.
+
+Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+Return the merged tree.
+
+Note: The merging process must start from the root nodes of both trees.
+
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode):
+        if root1 is None:
+            return root2
+        if root2 is None:
+            return root1
+    
+        newTree = TreeNode((root1.val + root2.val))
+        newTree.left = self.mergeTrees(root1.left, root2.left)
+        newTree.right = self.mergeTrees(root1.right, root2.right)
+        return newTree
+    
+            
+                
+        
+    
